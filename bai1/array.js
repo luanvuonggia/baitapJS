b5010c9d9ec6bed24798b20c6dc2c4e193ef61d4
dotranphuongnam <19520749@gm.uit.edu.vn>
2022-06-02 19:44:22 +0700

    Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch -

--- bai1/array.js
+++ bai1/array.js
@@ -16,7 +16,7 @@ students.shift();
 
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = ""; // TODO: CODE HERE
+const chuoiTen = students.join(" - "); // TODO: CODE HERE
 
 console.log('chuoiTen', chuoiTen);
 
