bdc5b3996f5467757241529f532e71e2c90c75ca
ynguyenntc <ynguyen.ntc@gmail.com>
2022-06-02 19:22:17 +0700

    Như Ý update array.js

--- bai1/array.js
+++ bai1/array.js
@@ -3,34 +3,67 @@ const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý'
 console.log(`Chiều dài của array students là ${students.lenght}`);
 
 // Tìm vị trí index của Anh Khoa 
+{
 const viTriCuaKhoa = ""; // TODO: CODE HERE
+for(let i = 0; i<students.length; i++)
+{
+    if(students[i] == 'Anh Khoa')
+    {
+        viTriCuaKhoa = i
+    }
+
+}
 console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
+}
 
 //Thêm Thành Phát vào array
-
-// TODO: CODE HERE
-
+{
+    students.push('Thành Phát')
+    console.log(students)
 // Xoá phần tử đầu tiên ra khỏi array
-
-// TODO: CODE HERE
-
+{
+    students.shift()
+    console.log(students)
+}
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
-
+{
 const chuoiTen = ""; // TODO: CODE HERE
+for(let i = 0; i<students.length; i++)
+{
+    if(i!= students.length- 1)
+    {
+        chuoiTen.push(students[i] + '-');
+    }
+    else{
+        chuoiTen.push(students[i]);
+    }
+
+}
 
 console.log('chuoiTen', chuoiTen);
-
+}
 //Xoá bỏ Như Ý ra khỏi array;
-
 // TODO: CODE HERE
+{
+    for(let i = 0; i<students.length; i++)
+    {
+        if(students[i] == "Như Ý")
+        {
+            students.splice(i, 1);
+        }
+    }
+    console.log(students)
+}
+
 
 // Cho 1 array mới
 
 const students2 = ['Duy Tín', 'Văn Thanh'];
 
 // nối student2 với student
-
 // TODO: CODE HERE
-
-console.log('Kết quả:');
-console.log(students);
+{
+    students = students.concat(students2); 
+    console.log('Kết quả:');
+    console.log(students);
+}
