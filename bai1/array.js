be9e6cb39f1f7e7d95b7be7e17767be79ceeb59c
trangtho <nguyenthitrangtho2512@gmail.com>
2022-06-02 19:49:24 +0700

    Tho update

--- bai1/array.js
+++ bai1/array.js
@@ -1,29 +1,33 @@
 const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý', 'Lâm Trường'];
 
-console.log(`Chiều dài của array students là ${students.lenght}`);
+console.log(`Chiều dài của array students là ${students.length}`);
 
 // Tìm vị trí index của Anh Khoa 
-const viTriCuaKhoa = ""; // TODO: CODE HERE
+const viTriCuaKhoa = students.indexOf("Anh Khoa"); // TODO: CODE HERE
 console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
 
 //Thêm Thành Phát vào array
 
 // TODO: CODE HERE
+students.push("Thành Phát");
 
 // Xoá phần tử đầu tiên ra khỏi array
 
 // TODO: CODE HERE
-
+students.shift();
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = ""; // TODO: CODE HERE
-
+const chuoiTen =  students.join(" - ");
 console.log('chuoiTen', chuoiTen);
 
 //Xoá bỏ Như Ý ra khỏi array;
 
-// TODO: CODE HERE
 
+// TODO: CODE HERE
+//const viTriremove = students.indexOf("Như Ý");
+
+const newarray = students.filter(item => item != "Như Ý" );
+console.log('chuoi moi: ', newarray);
 // Cho 1 array mới
 
 const students2 = ['Duy Tín', 'Văn Thanh'];
