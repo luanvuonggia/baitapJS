3a23557a2cacaa4695973eb63b8561c6a89940ed
PhuongLe222 <kkimphuong22@gmail.com>
2022-06-02 20:56:09 +0700

    Done string.js

--- bai1/string.js
+++ bai1/string.js
@@ -1,25 +1,28 @@
 let originalString = 'Lop hoc web cua Devera';
 
 //TODO: Tính độ dài của chuỗi
-const length = ''; //CODE HERE
+
+const length = originalString.length; //CODE HERE
 console.log(`Đội dài của chuỗi là ${length}`);
 
 // TODO: lấy ra chữ web bằng hàm slice
 
-const webString = ''; //CODE HERE
+const webString = originalString.slice(originalString.indexOf('web'),originalString.indexOf('web')+3); //CODE HERE
 
 console.log('webString', webString);
 
 //TODO: thay tất các các khoảng trống trong originalString bằng dấu phẩy
-//CODE HERE
+originalString = originalString.replace(/ /g, ',');
 
 console.log('originalString', originalString);
 
 // Viết 1 hàm kiểm tra xem chuỗi truyền vào có chứa từ 'Dev' hay không
 
 function checkIsDev(s) {
-    //CODE HERE
-    return true;
+    if (String(s).includes('Dev'))
+        return true;
+    else
+        return false;
 }
 
 console.log('Check is Dev', checkIsDev('123124njanad'));
