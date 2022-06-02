55a62ea31c83386713a202adcc98b684daea932f
Lam Truong <lamtruonq280902@gmail.com>
2022-06-02 20:18:34 +0700

    file cua lamtruong

--- bai1/array.js
+++ bai1/array.js
@@ -3,34 +3,36 @@ const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý'
 console.log(`Chiều dài của array students là ${students.lenght}`);
 
 // Tìm vị trí index của Anh Khoa 
-const viTriCuaKhoa = ""; // TODO: CODE HERE
+const viTriCuaKhoa = students.indexOf("Anh Khoa"); // TODO: CODE HERE
 console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
 
 //Thêm Thành Phát vào array
+students.push("Thành Phát");
 
-// TODO: CODE HERE
 
 // Xoá phần tử đầu tiên ra khỏi array
+students.unshift();
 
 // TODO: CODE HERE
 
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = ""; // TODO: CODE HERE
+const chuoiTen = students.join(" - "); // TODO: CODE HERE
 
 console.log('chuoiTen', chuoiTen);
 
 //Xoá bỏ Như Ý ra khỏi array;
+delete students[4];
 
-// TODO: CODE HERE
 
 // Cho 1 array mới
 
 const students2 = ['Duy Tín', 'Văn Thanh'];
 
 // nối student2 với student
+//students.push(...students2);
 
 // TODO: CODE HERE
 
 console.log('Kết quả:');
-console.log(students);
+console.log(students.concat(students2));
