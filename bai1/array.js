fe77824851115d2f955e5d290544cf5d7897b4b4
linhdatran3 <linhdatran3@gmail.com>
2022-06-02 20:11:27 +0700

    fix :bug:

--- bai1/array.js
+++ bai1/array.js
@@ -9,7 +9,7 @@ console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
 //Thêm Thành Phát vào array
 
 // TODO: CODE HERE
-students.push("Thành Phát");
+students.push('Thành Phát');
 
 // Xoá phần tử đầu tiên ra khỏi array
 
@@ -18,13 +18,13 @@ students.shift();
 
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = students.join(" - ")
+const chuoiTen = students.join(' - ')
 console.log('chuoiTen', chuoiTen);
 
 //Xoá bỏ Như Ý ra khỏi array;
 
 // TODO: CODE HERE
-students.splice(students.indexOf("Như Ý"),1);
+students.splice(students.indexOf('Như Ý'),1);
 
 
 // Cho 1 array mới
