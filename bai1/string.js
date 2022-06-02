7e8d4f14852f04e42580bf9134b8aa4a6bacce7a
N a h p o a H <nhuthao.tpcaolanh@gmail.com>
2022-06-02 21:02:36 +0700

    Commit string.js

--- bai1/string.js
+++ bai1/string.js
@@ -1,25 +1,30 @@
 let originalString = 'Lop hoc web cua Devera';
 
 //TODO: Tính độ dài của chuỗi
-const length = ''; //CODE HERE
+const length = originalString.length; //CODE HERE
 console.log(`Đội dài của chuỗi là ${length}`);
 
 // TODO: lấy ra chữ web bằng hàm slice
 
-const webString = ''; //CODE HERE
+const webString = originalString.slice(8, 12); //CODE HERE
 
 console.log('webString', webString);
 
 //TODO: thay tất các các khoảng trống trong originalString bằng dấu phẩy
 //CODE HERE
-
+originalString = originalString.split(" ").join(",");
 console.log('originalString', originalString);
 
 // Viết 1 hàm kiểm tra xem chuỗi truyền vào có chứa từ 'Dev' hay không
 
 function checkIsDev(s) {
-    //CODE HERE
-    return true;
+
+    if(s.includes("Dev") == true){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 console.log('Check is Dev', checkIsDev('123124njanad'));
