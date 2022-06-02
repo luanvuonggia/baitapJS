e7554fa3d20ac931b62cbd02d6c5f87636b57051
trungbabaok <trungmuok123@gmail.com>
2022-06-02 19:54:48 +0700

    quoctrung commit

--- bai1/array.js
+++ bai1/array.js
@@ -1,28 +1,33 @@
-const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý', 'Lâm Trường'];
+students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý', 'Lâm Trường'];
 
-console.log(`Chiều dài của array students là ${students.lenght}`);
+console.log(`Chiều dài của array students là ${students.length}`);
 
 // Tìm vị trí index của Anh Khoa 
-const viTriCuaKhoa = ""; // TODO: CODE HERE
+const viTriCuaKhoa = students.indexOf("Anh Khoa"); // TODO: CODE HERE
 console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
 
 //Thêm Thành Phát vào array
 
 // TODO: CODE HERE
+students.push("Thành Phát");
 
 // Xoá phần tử đầu tiên ra khỏi array
 
 // TODO: CODE HERE
+students.shift();
 
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = ""; // TODO: CODE HERE
+const chuoiTen = students.join(" - "); // TODO: CODE HERE
+
 
 console.log('chuoiTen', chuoiTen);
 
 //Xoá bỏ Như Ý ra khỏi array;
 
-// TODO: CODE HERE
+// TODO: CODE HERE 
+const new_array = students.filter((item) => item !== "Như Ý");
+console.log(new_array);
 
 // Cho 1 array mới
 
@@ -31,6 +36,7 @@ const students2 = ['Duy Tín', 'Văn Thanh'];
 // nối student2 với student
 
 // TODO: CODE HERE
+students = students.concat(students2);
 
 console.log('Kết quả:');
 console.log(students);
