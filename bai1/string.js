e96cc494e31b5c22a91568792ec648abf8ee3e9a
imgnourt <nvtruong125@gmail.com>
2022-06-02 21:00:24 +0700

    done string.js

--- bai1/string.js
+++ bai1/string.js
@@ -1,25 +1,31 @@
 let originalString = 'Lop hoc web cua Devera';
 
 //TODO: Tính độ dài của chuỗi
-const length = ''; //CODE HERE
+const length = originalString.length; //CODE HERE
 console.log(`Đội dài của chuỗi là ${length}`);
 
 // TODO: lấy ra chữ web bằng hàm slice
-
-const webString = ''; //CODE HERE
+let web="web";
+let leg= web.length
+let a= originalString.indexOf(web);
+const webString =originalString.slice(a,a+leg); //CODE HERE
 
 console.log('webString', webString);
 
 //TODO: thay tất các các khoảng trống trong originalString bằng dấu phẩy
 //CODE HERE
-
-console.log('originalString', originalString);
+originalString = originalString.split(" ").join(",");
+console.log('originalString:', originalString);
 
 // Viết 1 hàm kiểm tra xem chuỗi truyền vào có chứa từ 'Dev' hay không
 
 function checkIsDev(s) {
     //CODE HERE
+    if(s.includes("Dev")){
     return true;
+    }else{
+        return false;
+    }
 }
 
 console.log('Check is Dev', checkIsDev('123124njanad'));
