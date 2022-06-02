a27a8cd3b4a3d72fdeac91ce223bd1d0b91f5b09
me@example.com <me@example.com>
2022-06-02 19:14:54 +0700

    Tim vi tri cua Anh Khoa

--- bai1/array.js
+++ bai1/array.js
@@ -1,10 +1,15 @@
 const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý', 'Lâm Trường'];
 
-console.log(`Chiều dài của array students là ${students.lenght}`);
+var l = students.length;
+
+console.log('Chiều dài của array students là', l);
 
 // Tìm vị trí index của Anh Khoa 
-const viTriCuaKhoa = ""; // TODO: CODE HERE
-console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
+const viTriCuaKhoa = students.indexOf("Anh Khoa");
+
+console.log('Vị trí index của Anh Khoa là', viTriCuaKhoa);
+
+
 
 //Thêm Thành Phát vào array
 
