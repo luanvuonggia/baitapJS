117a94519a010d118e06d5a820f04322c41cb4f7
Trang Lê Thị Thu <19522382@ms.uit.edu.vn>
2022-06-02 20:07:18 +0700

    Done array.js

--- bai1/array.js
+++ bai1/array.js
@@ -1,6 +1,6 @@
 const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý', 'Lâm Trường'];
 
-console.log(`Chiều dài của array students là ${students.lenght}`);
+console.log(`Chiều dài của array students là ${students.length}`);
 
 // Tìm vị trí index của Anh Khoa 
 const viTriCuaKhoa = students.indexOf('Anh Khoa'); 
@@ -8,21 +8,24 @@ console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
 
 //Thêm Thành Phát vào array
 
-// TODO: CODE HERE
+students.push('Thành Phát');
 
 // Xoá phần tử đầu tiên ra khỏi array
 
-// TODO: CODE HERE
+students.shift();
 
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = ""; // TODO: CODE HERE
+
+const chuoiTen = students.join(" - ");  
 
 console.log('chuoiTen', chuoiTen);
 
 //Xoá bỏ Như Ý ra khỏi array;
 
-// TODO: CODE HERE
+const valueToRemove = "Như Ý";
+const new_arr = students.filter(item => item !== valueToRemove);
+console.log(new_arr);
 
 // Cho 1 array mới
 
@@ -30,7 +33,7 @@ const students2 = ['Duy Tín', 'Văn Thanh'];
 
 // nối student2 với student
 
-// TODO: CODE HERE
+students.push(...students2);
 
 console.log('Kết quả:');
 console.log(students);
