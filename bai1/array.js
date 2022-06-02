980cac9663a4b29e95fab8cee1809e24841fd813
N a h p o a H <nhuthao.tpcaolanh@gmail.com>
2022-06-02 19:34:28 +0700

    Commit bai tap array

--- bai1/array.js
+++ bai1/array.js
@@ -1,28 +1,29 @@
 const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý', 'Lâm Trường'];
 
-console.log(`Chiều dài của array students là ${students.lenght}`);
+console.log(`Chiều dài của array students là ${students.length}`);
 
 // Tìm vị trí index của Anh Khoa 
-const viTriCuaKhoa = ""; // TODO: CODE HERE
+const viTriCuaKhoa = students.indexOf('Anhh Khoa'); 
 console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
 
 //Thêm Thành Phát vào array
 
-// TODO: CODE HERE
-
+students.push("Thành Phát");
+console.log(students);
 // Xoá phần tử đầu tiên ra khỏi array
 
-// TODO: CODE HERE
+students.shift(students);
+console.log(students);
 
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = ""; // TODO: CODE HERE
-
+const chuoiTen = students.join(" - "); // TODO: CODE HERE
 console.log('chuoiTen', chuoiTen);
 
 //Xoá bỏ Như Ý ra khỏi array;
 
-// TODO: CODE HERE
+students.splice(3, 1);
+console.log(students);
 
 // Cho 1 array mới
 
@@ -30,7 +31,7 @@ const students2 = ['Duy Tín', 'Văn Thanh'];
 
 // nối student2 với student
 
-// TODO: CODE HERE
-
+const Finalstudents = students2.concat(students);
+console.log('Kết quả:', Finalstudents);
 console.log('Kết quả:');
 console.log(students);
