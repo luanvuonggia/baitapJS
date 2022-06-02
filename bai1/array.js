cd4b5b837ccd8a9c75a83bc39b908435be3713b2
Vu Hieu Hoang <you@example.com>
2022-06-02 19:27:38 +0700

    Noi bai

--- bai1/array.js
+++ bai1/array.js
@@ -1,36 +1,35 @@
 const students =  ['Gia Trân', 'Linh Đa', 'Anh Khoa', 'Khánh Hòa', 'Như Ý', 'Lâm Trường'];
 
 console.log(`Chiều dài của array students là ${students.lenght}`);
-
 // Tìm vị trí index của Anh Khoa 
-const viTriCuaKhoa = ""; // TODO: CODE HERE
+const viTriCuaKhoa = students.indexOf("Anh Khoa"); // TODO: CODE HERE
 console.log(`Vị trí index của Anh Khoa là ${viTriCuaKhoa}`);
 
 //Thêm Thành Phát vào array
 
-// TODO: CODE HERE
+students[6] = 'Thành Phát'
 
 // Xoá phần tử đầu tiên ra khỏi array
 
-// TODO: CODE HERE
+students.shift()
 
 // Tạo một string mới có chứa tên của các bạn trong array và nối với nhau bằng dấu gạch - ( ví dụ Gia Trân - Linh Đa - Anh Khoa - ...)
 
-const chuoiTen = ""; // TODO: CODE HERE
+const chuoiTen = students.join(' - '); // TODO: CODE HERE
 
 console.log('chuoiTen', chuoiTen);
 
 //Xoá bỏ Như Ý ra khỏi array;
-
-// TODO: CODE HERE
+let ValuetoRemove = 'Như Ý'
+const NewArray = students.filter(item => item !== ValuetoRemove)
 
 // Cho 1 array mới
-
+console.log(NewArray)
 const students2 = ['Duy Tín', 'Văn Thanh'];
 
 // nối student2 với student
 
-// TODO: CODE HERE
+students.concat(students2)
 
 console.log('Kết quả:');
 console.log(students);
