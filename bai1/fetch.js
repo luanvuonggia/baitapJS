5f9f4876ff387867ce6583c5ce28f3e75a440591
dinhtoan-uit <18521504@gm.uit.edu.vn>
2022-06-05 17:41:54 +0700

    Done bai fetch.js

--- bai1/fetch.js
+++ bai1/fetch.js
@@ -1,19 +1,29 @@
 
 // API https://randomuser.me/api/?results=5 results = 5 là số lượng users trả về 
-
+import fetch from "node-fetch";
 let users = [];
 async function getUsers() {
     const users = await fetch("https://randomuser.me/api/?results=5");
     return users.json();
-  }
-getUsers().then((data) => {
-    users = data?.results;
+}
+await getUsers().then((data) => {
+    users = data?.results
 });
 
 // Lọc ra những bạn là nữ
 function filterFemale(users) {
     // Code here
-    return [];
+    let female = [];
+    // Cach 1
+    // for (let i = 0; i < users.length; i++) {
+    //     if (users[i]["gender"] == "female") {
+    //         female.push(users[i]);
+    //     }
+    // }
+
+    // Cach 2
+    female = users.filter(obj => obj.gender == 'female');
+    return female;
 }
 
 const femaleList = filterFemale(users);
@@ -22,7 +32,17 @@ const femaleList = filterFemale(users);
 
 function getEmail(users) {
     // Code here
-    return [];
+    let email = [];
+    // Cach 1
+    // for (let i = 0; i < users.length; i++) {
+    //     if (users[i]["email"] != null) {
+    //         email.push(users[i]["email"]);
+    //     }
+    // }
+
+    // Cach 2
+    email = users.map(({ email }) => email);
+    return email;
 }
 
 const email = getEmail(users);
