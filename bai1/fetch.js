9abc000f588238c26b3d42186fd696f74566f1ac
DangDaiMinhChauUIT <dangdaiminhchau0901@gmail.com>
2022-06-08 23:56:04 +0700

    Done ex fetch.js

--- bai1/fetch.js
+++ bai1/fetch.js
@@ -2,27 +2,31 @@
 // API https://randomuser.me/api/?results=5 results = 5 là số lượng users trả về 
 
 let users = [];
+import fetch from 'node-fetch';
 async function getUsers() {
     const users = await fetch("https://randomuser.me/api/?results=5");
     return users.json();
   }
-getUsers().then((data) => {
+await getUsers().then((data) => {
     users = data?.results;
 });
 
 // Lọc ra những bạn là nữ
 function filterFemale(users) {
     // Code here
-    return [];
+    return users.filter(user => user.gender == "female");
 }
 
 const femaleList = filterFemale(users);
+console.log(femaleList);
 
 // Tạo ra một array chứa tất cả email của user
 
 function getEmail(users) {
     // Code here
-    return [];
+    return users.map(user => user.email);
 }
 
 const email = getEmail(users);
+console.log("\nList mail: \n");
+console.log(email);
