154e0d55b7acb2ab9930ab15f1fa9ffb3b90c661
linhdatran3 <linhdatran3@gmail.com>
2022-06-09 15:02:48 +0700

    completed file fetch.js :tada:

--- bai1/fetch.js
+++ bai1/fetch.js
@@ -1,28 +1,40 @@
-
-// API https://randomuser.me/api/?results=5 results = 5 là số lượng users trả về 
+// API https://randomuser.me/api/?results=5 results = 5 là số lượng users trả về
 
 let users = [];
 async function getUsers() {
-    const users = await fetch("https://randomuser.me/api/?results=5");
-    return users.json();
-  }
+  const users = await fetch("https://randomuser.me/api/?results=5");
+  return users.json();
+}
+
 getUsers().then((data) => {
-    users = data?.results;
+  users = data?.results;
+  console.log(users);
+  // Lọc ra những bạn là nữ
+  function filterFemale(users) {
+    // Code here
+    try {
+      const tmp = users.filter((user) => user.gender == "female");
+      //console.log(tmp);
+      return tmp;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const femaleList = filterFemale(users);
+  console.log(femaleList);
+  // Tạo ra một array chứa tất cả email của user
+
+  function getEmail(users) {
+    // Code here
+    let tmp = [];
+    users.forEach((user) => {
+      tmp += user.email;
+    });
+    //console.log(tmp);
+    return tmp;
+  }
+
+  const email = getEmail(users);
+  console.log(email);
 });
-
-// Lọc ra những bạn là nữ
-function filterFemale(users) {
-    // Code here
-    return [];
-}
-
-const femaleList = filterFemale(users);
-
-// Tạo ra một array chứa tất cả email của user
-
-function getEmail(users) {
-    // Code here
-    return [];
-}
-
-const email = getEmail(users);
