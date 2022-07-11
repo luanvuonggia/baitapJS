eeb66891e1c3baff94e88111d2af83acfd9990b7
PhuongLe222 <kkimphuong22@gmail.com>
2022-07-11 10:04:08 +0700

    fix merge

--- bai1/string.js
+++ bai1/string.js
@@ -1,29 +1,17 @@
 let originalString = 'Lop hoc web cua Devera';
 
 //TODO: Tính độ dài của chuỗi
-<<<<<<< HEAD
-
-const length = originalString.length; //CODE HERE
-=======
 const length = originalString.length;
->>>>>>> 9bb173ffc7ad4f8cd3510b1bdb6ca10c5803917a
 console.log(`Đội dài của chuỗi là ${length}`);
 
 // TODO: lấy ra chữ web bằng hàm slice
 
-<<<<<<< HEAD
 const webString = originalString.slice(originalString.indexOf('web'),originalString.indexOf('web')+3); //CODE HERE
-=======
-const webString = originalString.slice(8,11);
->>>>>>> 9bb173ffc7ad4f8cd3510b1bdb6ca10c5803917a
 
 console.log('webString', webString);
 
 //TODO: thay tất các các khoảng trống trong originalString bằng dấu phẩy
-<<<<<<< HEAD
 originalString = originalString.replace(/ /g, ',');
-=======
->>>>>>> 9bb173ffc7ad4f8cd3510b1bdb6ca10c5803917a
 
 // console.log('originalString', originalString.split(' ').join(','));
 console.log('originalString', originalString.replaceAll(' ', ','));
@@ -32,13 +20,9 @@ console.log('originalString', originalString.replaceAll(' ', ','));
 
 function checkIsDev(s) {
     if (s.includes('Dev'))
-<<<<<<< HEAD
         return true;
     else
         return false;
-=======
-    return true;
->>>>>>> 9bb173ffc7ad4f8cd3510b1bdb6ca10c5803917a
 }
 
 console.log('Check is Dev', checkIsDev('123124njanad'));
